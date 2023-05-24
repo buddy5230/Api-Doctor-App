6b0881a5608d6529eb1ca248fee6bd16df3bf9c4
buddy5230 <74503111+buddy5230@users.noreply.github.com>
2023-05-25 00:23:22 +0700

    change port

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // PORT
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
     console.log('Connected to port ' + port)
 })
