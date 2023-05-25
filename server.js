5be99d51c13642f6563f36bd804b281fb5442c2c
buddy5230 <74503111+buddy5230@users.noreply.github.com>
2023-05-25 16:03:26 +0700

    update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 mongoose.connect(dbConfig.db, {
     useNewUrlParser: true,
-    // useUnifiedTopology: true
+    
 }).then(() => {
     console.log('Database successfully connected');
 }, 
@@ -36,65 +36,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log("Node Server has Started at Port " + port);
 });
-/*let express = require('express'),
-    mongoose = require('mongoose'),
-    cors = require('cors'),
-    bodyParser = require('body-parser'),
-    dbConfig = require('./database/db');
-    // eslint-disable-next-line no-undef
-    path = require('path');
-    const app = express();
-// Express Route
-const userRoute = require('../doctor-api/route/user_route');
-
-// Connecting MongDB Database
-mongoose.Promise = global.Promise;
-
-mongoose.connect(dbConfig.db, {
-    useNewUrlParser: true,
-    // useUnifiedTopology: true
-}).then(() => {
-    console.log('Database successfully connected');
-}, 
-    error => {
-        console.log('Could not connect to database: ' + error)
-    }
-)
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
-app.use(cors());
-app.use('/users', userRoute);
-// PORT
-const port = process.env.PORT || 5000;
-const server = app.listen(port, () => {
-    console.log('Connected to port ' + port)
-})
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../build/index.html"))
-    })
-}
-// 404 Error
-app.use((req, res, next) => {
-    //
-    // eslint-disable-next-line no-undef
-    next(createError(404))
-})
-
-
-app.use(function(err, req, res, next) {
-    console.error(err.message);
-    if (!err.statusCode) err.statusCode = 500;
-    res.status(err.statusCode).send(err.message);
-})
-/*
-// "server": "nodemon server",
-"test": "echo \"Error: no test specified\" && exit 1",
-"main": "./route/user.route.js",
-*/ 
